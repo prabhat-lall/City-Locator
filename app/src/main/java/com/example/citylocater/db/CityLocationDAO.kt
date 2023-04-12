6d74d43d4217c29b4f6f48705b80ba6c121e4814
Prabhat Kumar <pkrlal883@gmail.com>
2023-04-12 21:38:06 +0530

    Adding Database flow

---
+++ app/src/main/java/com/example/citylocater/db/CityLocationDAO.kt
@@ -0,0 +1,21 @@
+package com.example.citylocater.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.citylocater.models.CityLocationItem
+
+
+@Dao
+interface CityLocationDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+   fun insertCityLocationList(cityLocationItem: List<CityLocationItem>)
+
+    @Query("SELECT * FROM CITY_LOCATION_TABLE")
+    fun getCityLocationList(): List<CityLocationItem>
+
+
+}
